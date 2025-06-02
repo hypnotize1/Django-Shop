806ca446fe5ca90547fa7c7e722f21734ecb90b1
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-06-02 14:13:53 +0330

    Add Category, Brand, Product models with slug and stock management

--- shopnest/settings.py
+++ shopnest/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store.apps.StoreConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media setting
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
---
+++ store/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.utils.text import slugify
+
+def unique_slug_generator(instance, slug_field_name, value_to_slugify):
+    slug = slugify(value_to_slugify)
+    modelclass = instance.__class__
+    unique_slug = slug
+    num = 1
+
+    while modelclass.objects.filter(**{slug_field_name: unique_slug}).exists():
+        unique_slug = f"{slug}-{num}"
+        num += 1
+
+    return unique_slug
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slug_generator(self, 'slug', self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slug_generator(self, 'slug', self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slug_generator(self, 'slug', self.name)
+        if self.stock == 0:
+            self.available = False
+        else:
+            self.available = True
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
