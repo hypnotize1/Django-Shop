7ccfd27384a4923767f38fa1b3a584aad98ff917
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-06-03 13:21:13 +0330

    Change database from sqlite to mysql

--- shopnest/settings.py
+++ shopnest/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'shopnest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store_db',
+        'USER': 'root',
+        'PASSWORD': 'MyNewPass123!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
