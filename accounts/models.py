70cd9378cc8eb70790ed25c5b38f99feeb9d485d
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-13 17:47:29 +0330

    change field name to full_name

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 
+from accounts.validators import phone_validator
+
+
 # Create your models here.
 class AccountManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, name, phone, password, **extra_fields):
-        values = [email, name, phone]
+    def _create_user(self, email, full_name, phone, password, **extra_fields):
+        values = [email, full_name, phone]
         field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
         for field_name, value in field_value_map.items():
             if not value:
@@ -16,7 +19,7 @@ class AccountManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(
             email=email,
-            name=name,
+            full_name=full_name,
             phone=phone,
             **extra_fields
         )
@@ -24,12 +27,12 @@ class AccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, name, phone, password=None, **extra_fields):
+    def create_user(self, email, full_name, phone, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, name, phone, password, **extra_fields)
+        return self._create_user(email, full_name, phone, password, **extra_fields)
 
-    def create_superuser(self, email, name, phone, password=None, **extra_fields):
+    def create_superuser(self, email, full_name, phone, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -38,14 +41,14 @@ class AccountManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, name, phone, password, **extra_fields)
+        return self._create_user(email, full_name, phone, password, **extra_fields)
 
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=150)
-    phone = models.CharField(max_length=11)
+    full_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=11, validators=[phone_validator], unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     picture = models.ImageField(null=True, blank=True)
     is_staff = models.BooleanField(default=False)
@@ -56,11 +59,13 @@ class Account(AbstractBaseUser, PermissionsMixin):
     objects = AccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phone']
+    REQUIRED_FIELDS = ['full_name', 'phone']
 
     def get_full_name(self):
-        return self.name
+        return self.full_name
 
     def get_short_name(self):
-        return self.name.split()[0]
+        return self.full_name.split()[0]
 
+    def __str__(self):
+        return self.email
