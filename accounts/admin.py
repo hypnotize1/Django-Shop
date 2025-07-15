39c70ed519986cfe244b36491db5e955b472de51
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-15 16:58:18 +0330

    custom-register custom user model in admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,33 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserCreationForm
 
 from .models import Account
 from .forms import UserChangeForm, AdminUserCreationForm
+
 
 # Register your models here.
 
 class AccountAdmin(BaseUserAdmin):
     form = UserChangeForm
-    add_form = UserCreationForm
+    add_form = AdminUserCreationForm
 
-    list_display = ('email', 'name', 'phone', 'date_of_birth', 'is_staff',  'is_superuser')
+    list_display = ('email', 'full_name', 'phone', 'date_of_birth', 'is_staff', 'is_superuser')
     list_filter = ('is_superuser',)
 
     fieldsets = (
         (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password')}),
-        ('Personal info', {'fields': ('name', 'phone', 'date_of_birth', 'picture')}),
+        ('Personal info', {'fields': ('full_name', 'phone', 'date_of_birth', 'picture')}),
         ('Groups', {'fields': ('groups',)}),
         ('Permissions', {'fields': ('user_permissions',)}),
     )
     add_fieldsets = (
         (None, {'fields': ('email', 'is_staff', 'is_superuser', 'password1', 'password2')}),
-        ('Personal info', {'fields': ('name', 'phone', 'date_of_birth', 'picture')}),
+        ('Personal info', {'fields': ('full_name', 'phone', 'date_of_birth', 'picture')}),
         ('Groups', {'fields': ('groups',)}),
         ('Permissions', {'fields': ('user_permissions',)}),
     )
 
-    search_fields = ('email', 'name', 'phone')
+    search_fields = ('email', 'full_name', 'phone')
     ordering = ('email',)
     filter_horizontal = ()
 
