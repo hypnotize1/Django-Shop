8af6da4bffc274cfb4bfa5c7056f5b871f74d454
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-15 16:56:58 +0330

    add orders app urls to project

--- shopnest/urls.py
+++ shopnest/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('orders/', include('orders.urls', namespace='orders'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
