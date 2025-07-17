6b8dfd516bd1594a0799690fd0a37c7b0cac8a24
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-17 16:26:58 +0330

    write Cart class for adding cart to session. views for adding and showing cart details,templates and urls

---
+++ orders/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.IntegerField(min_value=1, max_value=10, initial=1)
+    override = forms.BooleanField(
+    required=False,
+    initial=False,
+    widget=forms.HiddenInput
+)
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from store.models import Product
 from django.views import View
-
+from django.views.generic import FormView, TemplateView
+from .forms import CartAddProductForm
+from .cart import Cart
 # Create your views here.
 
-class CartView(View):
-    pass
+class CartView(TemplateView):
+    template_name = 'orders/cart_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = Cart(self.request)
+        context['cart'] = cart
+        total = sum(item['total_price'] for item in cart)
+        context['total'] = total
+        return context
 
+class CartAddView(FormView):
+    form_class = CartAddProductForm
+    template_name = 'orders/cart_add.html' 
+    
+    def form_valid(self, form):    
+        cart = Cart(self.request)
+        product = get_object_or_404(Product, id=self.kwargs['product_id'])
+        cd = form.cleaned_data
+        cart.add(product, quantity=cd['quantity'], override_quantity=cd['override'])
+        return redirect('orders:cart_detail')
+
+    
+    
