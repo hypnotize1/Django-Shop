a8fb3ab1ce7989a5754f1426d4452d82898e24c1
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-22 18:46:18 +0330

    form for create order and check if payment was successful remove pay button

--- orders/forms.py
+++ orders/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from .models import Order
 
 class CartAddProductForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=10, initial=1)
@@ -8,3 +8,26 @@ class CartAddProductForm(forms.Form):
     initial=False,
     widget=forms.HiddenInput
 )
+
+# orders/forms.py
+from django import forms
+from .models import Order
+from store.models import Address
+
+class OrderCreationForm(forms.ModelForm):
+    shipping_address = forms.ModelChoiceField(queryset=Address.objects.none(),
+                                              empty_label="Select shipping address")
+
+    class Meta:
+        model = Order
+        fields = ['shipping_address', 'payment_method']
+        widgets = {
+            'payment_method': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['shipping_address'].queryset = Address.objects.filter(user=user)
