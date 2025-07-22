d2c21acc2eb430355519b35777d311bdc7f463cf
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-22 18:35:18 +0330

    1-add payment_ref_id for save ref ids in database, 2-change payment_method to choice field, 3-write write generator for generate random order-number in change save method for check if order-number didnt insert will generate automatically

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from accounts.models import Account
 from store.models import Address, Product, ProductVariant
+from django.utils.timezone import now
+from random import randint
 
 # Create your models here.
 class Order(models.Model):
@@ -19,7 +21,12 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
-    payment_method = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=50, choices=[
+        ('cash_on_delivery', 'Cash on Delivery'),
+        ('online_payment', 'Online Payment'),
+        ('wallet_payment', 'Wallet Payment'),
+    ])
+    payment_ref_id = models.CharField(max_length=100, blank=True, null=True)  # 👈 اضافه کن
 
     def __str__(self):
         return f"Order #{self.order_number}"
@@ -27,6 +34,17 @@ class Order(models.Model):
     def get_total_order_price(self):
         return sum(item.get_total_item_price() for item in self.items.all())
 
+    
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self.generate_unique_order_number()
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_unique_order_number():
+        from random import randint
+        from django.utils.timezone import now
+        return f"ORD{now().strftime('%Y%m%d')}{randint(1000,9999)}"
  
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
