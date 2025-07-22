9efb8f1b756c3b03d22dd61bcca769152cb6afd7
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-07-22 18:41:09 +0330

    pay and verify urls for zarinpal

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
+    path('create/', views.OrderCreateView.as_view(), name = 'order_create'),
+    path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name = 'order_detail'),
     path('cart/', views.CartView.as_view(), name='cart_detail'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),  
-    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart-remove')    
+    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
+    path('pay/<int:order_id>/', views.PayOrderView.as_view(), name='pay_order'),
+    path('pay/verify/', views.PaymentVerifyView.as_view(), name='payment_verify'),
 ]
